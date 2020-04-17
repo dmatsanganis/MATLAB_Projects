63f811e74e863f101aeae0bee71213e4eb5bf08b
dimitrismatsanganis <jimmatsa@hotmail.gr>
2020-04-17 14:48:49 +0300

    Add comments.

--- Two_Dimensional_Normal_Distributions.m
+++ Two_Dimensional_Normal_Distributions.m
@@ -30,25 +30,37 @@ ylabel('x_2');
 % R1 = |x12_min x12_max| and R2 = |x22_min x22_max|
 R1 = minmax(C1');
 R2 = minmax(C2');
+
+% Plot the two-dimensional probabibility denstity functions.
 PlotTwoDimensionalGaussians(R1,R2,MU1,MU2,SIGMA,SIGMA);
+
+% Set the minimum and maximum values for the p parameter of the covariance
+% matrix.
 p_min = -1;
 p_max = 1;
+% Set the step parameter for the parameter p.
 dp = 0.05;
+% Set the range of the parameter p.
 p_range = p_min:dp:p_max;
 
 % Initialize Figure.
 figure('Name','Capture P Ranging');
+
 for p = p_range
     clf
     axis equal
+    % Set the covariance matrix for both classes.
     SIGMA = [1 p;p 1];
+    % Generate samples for both classes.
     C1 = mvnrnd(MU1,SIGMA,N);
     C2 = mvnrnd(MU2,SIGMA,N);
+    % Plot the data samples from both distributions.
     hold on
     plot(C1(:,1),C1(:,2),'or','LineWidth',2);
     plot(C2(:,1),C2(:,2),'og','LineWidth',2);
     grid on
     xlabel('x_1');
     ylabel('x_2');
+    % Pause for a little.
     pause(0.1);
 end
