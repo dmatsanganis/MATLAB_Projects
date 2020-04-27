fceeb8fbd66d2f8cef51027223aae5a598494ca0
dimitrismatsanganis <jimmatsa@hotmail.gr>
2020-04-27 13:54:09 +0300

    Add comments vol3.

--- Cauchy_Round.m
+++ Cauchy_Round.m
@@ -34,7 +34,11 @@ function [Rc] = Cauchy_Round(mu,c,n)
 % x = F   (y) = c * tan(pi*(y - 0.5)) + mu [3]
 
 Ru = rand(1,n);
+% Generate the (n) uniform random samples that will be interpreted as the
+% values of F.
 
 Rc = c * tan(pi*(Ru - 0.5)) + mu;
+% Generate the (n) random samples from the Cauchy distribution by using
+% equation (3).
 
 end
