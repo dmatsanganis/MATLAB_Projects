57ab0d2229c4a84b5835c3eb947b891e735c9415
dimitrismatsanganis <jimmatsa@hotmail.gr>
2020-04-30 23:06:15 +0300

    Update Cauchy_Binary_Classification_Simulation.m

--- Cauchy_Binary_Classification_Simulation.m
+++ Cauchy_Binary_Classification_Simulation.m
@@ -1,1 +1,9 @@
-
+% Set the a-priori probabilities for the two classes by initializing 
+% variable P.
+P = 0.9;
+% Define the location parameters mu1 and mu2.
+MU1 = -2.0;
+MU2 = 2.0;
+% Define the scale parameter C1 and C2.
+C1 = 1.0;
+C2 = 1.0;
