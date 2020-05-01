3be63bf232854e5035b5ede7f53b76e2595917aa
dimitrismatsanganis <jimmatsa@hotmail.gr>
2020-05-01 22:15:52 +0300

    Update Cauchy_Binary_Classification_Simulation.m

--- Cauchy_Binary_Classification_Simulation.m
+++ Cauchy_Binary_Classification_Simulation.m
@@ -8,6 +8,16 @@ MU2 = 2.0;
 C1 = 1.0;
 C2 = 1.0;
 
+% Determine the intersection points of the two curves Pw1_x(x) and Pw2_x(x).
+% To this end, we need to obtain the roots of the following equation with
+% respect to x:
+% G(x) = Pw1_x(x) - Pw2_x(x) = 0 <=>
+% G(x) = P * Px_w1(x) - (1-P) * Px_w2(x) = 0 <=>
+% G(x) = (P-1) * C2 * (x - MU1)^2 + P * C1 * (x - MU2)^2 + (P-1)*C1^2*C2 + P*C2^2*C1 = 0 [4]
+
+% Define the necessary symbolic variables for computing the intesection
+% points
+
 syms mu1 mu2 p c1 c2 x
 G = (p-1) * c2 * (x - mu1)^2 + p * c1 * (x - mu2)^2 + (p-1)*c1^2*c2 + p*c2^2*c1;
 % Subsitite the symbolic variables appearing in the expression for G(x)
