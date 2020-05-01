e9153a619cf0889e17bf53acf65e7173f85a6f0f
dimitrismatsanganis <jimmatsa@hotmail.gr>
2020-05-01 22:14:40 +0300

    Update Cauchy_Binary_Classification_Simulation.m

--- Cauchy_Binary_Classification_Simulation.m
+++ Cauchy_Binary_Classification_Simulation.m
@@ -12,5 +12,8 @@ syms mu1 mu2 p c1 c2 x
 G = (p-1) * c2 * (x - mu1)^2 + p * c1 * (x - mu2)^2 + (p-1)*c1^2*c2 + p*c2^2*c1;
 
 Go = subs(G,[mu1 mu2 p c1 c2],[MU1 MU2 P C1 C2]);
+% Perform numeric operations involving the coefficients of the symbolic 
+% objects to simplify the expression for Go. The resulting Go variable will
+% remain a symbolic object.
 
 Go = vpa(Go);
