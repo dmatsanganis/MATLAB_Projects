2835383b173941dc0a0f4d986b7bd3a90fbbb6f9
dimitrismatsanganis <jimmatsa@hotmail.gr>
2020-05-01 22:13:30 +0300

    Update Cauchy_Binary_Classification_Simulation.m

--- Cauchy_Binary_Classification_Simulation.m
+++ Cauchy_Binary_Classification_Simulation.m
@@ -7,3 +7,9 @@ MU2 = 2.0;
 % Define the scale parameter C1 and C2.
 C1 = 1.0;
 C2 = 1.0;
+
+syms mu1 mu2 p c1 c2 x
+G = (p-1) * c2 * (x - mu1)^2 + p * c1 * (x - mu2)^2 + (p-1)*c1^2*c2 + p*c2^2*c1;
+
+Go = subs(G,[mu1 mu2 p c1 c2],[MU1 MU2 P C1 C2]);
+
