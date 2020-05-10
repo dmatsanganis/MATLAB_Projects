96a01367e066be0ff2f3fe4df39d512d4a55a810
dimitrismatsanganis <jimmatsa@hotmail.gr>
2020-05-10 19:04:43 +0300

    Update Cauchy_Binary_Classification_Simulation.m

--- Cauchy_Binary_Classification_Simulation.m
+++ Cauchy_Binary_Classification_Simulation.m
@@ -8,14 +8,16 @@ MU2 = 2.0;
 C1 = 1.0;
 C2 = 1.0;
 
-% Determine the intersection points of the two curves Pw1_x(x) and Pw2_x(x).
-% To this end, we need to obtain the roots of the following equation with
-% respect to x:
-% G(x) = Pw1_x(x) - Pw2_x(x) = 0 <=>
-% G(x) = P * Px_w1(x) - (1-P) * Px_w2(x) = 0 <=>
-% G(x) = (P-1) * C2 * (x - MU1)^2 + P * C1 * (x - MU2)^2 + (P-1)*C1^2*C2 + P*C2^2*C1 = 0 [4]
-% Define the necessary symbolic variables for computing the intesection
-% points
+%-----------------------------------------------------------------------------------------------
+% Determine the intersection points of the two curves Pw1_x(x) and Pw2_x(x).                   |
+% To this end, we need to obtain the roots of the following equation with                      |      
+% respect to x:                                                                                |                 
+% G(x) = Pw1_x(x) - Pw2_x(x) = 0 <=>                                                           |     
+% G(x) = P * Px_w1(x) - (1-P) * Px_w2(x) = 0 <=>                                               |     
+% G(x) = (P-1) * C2 * (x - MU1)^2 + P * C1 * (x - MU2)^2 + (P-1)*C1^2*C2 + P*C2^2*C1 = 0 [4]   | 
+% Define the necessary symbolic variables for computing the intesection                        |
+% points                                                                                       |
+%-----------------------------------------------------------------------------------------------
 
 syms mu1 mu2 p c1 c2 x
 G = (p-1) * c2 * (x - mu1)^2 + p * c1 * (x - mu2)^2 + (p-1)*c1^2*c2 + p*c2^2*c1;
@@ -54,31 +56,27 @@ end
 % Cmax = max{C1,C2} [8]
 % X = [MUmin - K*Cmax,MUmax + K*Cmax] [9]
 
-% It is easy to deduce that in the case where P(W1) = P(W2) and C1 = C2, 
-% then there exists a unique intersection point for the curves Pw1_x(x) and 
-% Pw2_x(x) which is given by the following equation:
-%                  xo = (1/2) * (MU1 + MU2) [10], 
-% which will necessarily lie within the interval [Xmin,Xmax].
-% However, in the case where P(W1) <> P(W2) and / or C1 <> C2, then there
-% exist two intersection points for the curves Pw1_x(x) and Pw2_x(x) which
-% are given by equation (4) and are stored in variables xo_min and xo_max.
-% In this case, we need to ensure that the ragne of the feature space that
-% we consider includes points xo_min and xo_max, that is we need to enforce
-% that: Xmin <= xo_min [11] and Xmax >= xo_max [12].
+%-----------------------------------------------------------------------------------------
+% It is easy to deduce that in the case where P(W1) = P(W2) and C1 = C2,                 |                  
+% then there exists a unique intersection point for the curves Pw1_x(x) and              |
+% Pw2_x(x) which is given by the following equation:                                     |
+%                  xo = (1/2) * (MU1 + MU2) [10],                                        |           
+% which will necessarily lie within the interval [Xmin,Xmax].                            |                       
+% However, in the case where P(W1) <> P(W2) and / or C1 <> C2, then there                |       
+% exist two intersection points for the curves Pw1_x(x) and Pw2_x(x) which               |               
+% are given by equation (4) and are stored in variables xo_min and xo_max.               |                                   
+% In this case, we need to ensure that the ragne of the feature space that               |                           
+% we consider includes points xo_min and xo_max, that is we need to enforce              |                               
+% that: Xmin <= xo_min [11] and Xmax >= xo_max [12].                                     |                                       
+%                                                                                        |                                   
+% In this setting equation (11) yields:                                                  |                   
+%         MUmin - K * Cmax <= xo_min ==> K >= (MUmin - xo_min) / Cmax [13]               |                   
+% Likewise, equation (12) yields:                                                        |                       
+%         MUmax + K * Cmax >= xo_max ==> K >= (xo_max - MUmax) / Cmax [14]               |                       
+% Combining inequalities (13) and (14) gives:                                            |                                   
+% K >= max{(MUmin - xo_min) / Cmax,(xo_max - MUmax) / Cmax} [15]                         |                           
+%-----------------------------------------------------------------------------------------
 
-% In this setting equation (11) yields:
-%         MUmin - K * Cmax <= xo_min ==> K >= (MUmin - xo_min) / Cmax [13]
-% Likewise, equation (12) yields:
-%         MUmax + K * Cmax >= xo_max ==> K >= (xo_max - MUmax) / Cmax [14]
-% Combining inequalities (13) and (14) gives:
-% K >= max{(MUmin - xo_min) / Cmax,(xo_max - MUmax) / Cmax} [15]
-
-% In this setting equation (11) yields:
-%         MUmin - K * Cmax <= xo_min ==> K >= (MUmin - xo_min) / Cmax [13]
-% Likewise, equation (12) yields:
-%         MUmax + K * Cmax >= xo_max ==> K >= (xo_max - MUmax) / Cmax [14]
-% Combining inequalities (13) and (14) gives:
-% K >= max{(MUmin - xo_min) / Cmax,(xo_max - MUmax) / Cmax} [15]
 Cmax = max(C);
 if(No==1)
     K = 10;
